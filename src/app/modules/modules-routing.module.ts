a0c99a8a0019f8e773aadc60bda7bbcf9e5df010
Ashleigh Christopher <ashleighc207@gmail.com>
2020-01-20 16:37:29 -0500

    implement lazy loading

--- src/app/modules/modules-routing.module.ts
+++ src/app/modules/modules-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ModulesHomeComponent } from "./modules-home/modules-home.component";
 
-const routes: Routes = [{ path: "modules", component: ModulesHomeComponent }];
+const routes: Routes = [{ path: "", component: ModulesHomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
