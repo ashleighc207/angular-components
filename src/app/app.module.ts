bf3dacd86e56534a56aec0f1da1cb11bd0a850ba
Ashleigh Christopher <ashleighc207@gmail.com>
2020-01-20 15:08:00 -0500

    get basic project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ElementsModule } from "./elements/elements.module";
+import { CollectionsModule } from "./collections/collections.module";
+import { ViewsModule } from "./views/views.module";
+import { ModulesModule } from "./modules/modules.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule,
+    ViewsModule,
+    ModulesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/modules/modules.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { ModulesRoutingModule } from "./modules-routing.module";
+import { ModulesHomeComponent } from "./modules-home/modules-home.component";
+
+@NgModule({
+  declarations: [ModulesHomeComponent],
+  imports: [CommonModule, ModulesRoutingModule],
+  exports: [ModulesHomeComponent]
+})
+export class ModulesModule {}
