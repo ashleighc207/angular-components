00cda02ffc1ed305533c09b46ca6f18daa4d7afd
Ashleigh Christopher <ashleighc207@gmail.com>
2020-01-20 16:07:05 -0500

    adjust box shadow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,18 @@ import { ElementsModule } from "./elements/elements.module";
 import { CollectionsModule } from "./collections/collections.module";
 import { ViewsModule } from "./views/views.module";
 import { ModulesModule } from "./modules/modules.module";
+import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ElementsModule,
     CollectionsModule,
     ViewsModule,
-    ModulesModule
+    ModulesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
